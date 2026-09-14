226a71282d39dbbeccc30468ed6a49ce09b47daf
martinaradkovicova-lgtm <martinaradkovicova@gmail.com>
2026-09-14 12:57:03 +0200

    header file

---
+++ ft_push_swap.h
@@ -0,0 +1,23 @@
+#ifndef FT_PUSH_SWAP_H
+# define FT_PUSH_SWAP_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include "libft.h"
+
+typedef struct s_num
+{
+	int	value;
+	int	index;
+	struct s_num *next;
+	struct s_num *prev;
+} t_num;
+
+typedef struct s_stack
+{
+	t_num	*top;
+	t_num	*bottom;
+	int		size;
+} t_stack
+
+#endif
